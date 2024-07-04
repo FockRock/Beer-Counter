84b4c8b83e80b5d3f107069a01d8eb03f3772829
FockRock <smirnov_nikita@mail.ru>
2024-07-04 19:30:10 +0300

    Basic app created

--- app/src/main/java/com/example/beercounter/MainActivity.kt
+++ app/src/main/java/com/example/beercounter/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.beercounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var i = 0
+        bindingClass.imageView.setOnClickListener {
+            bindingClass.textView.text = i++.toString()
         }
     }
 }
